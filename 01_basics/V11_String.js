5b7369c6c0f44de1c2b4d1b5921f8c056633e501
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2024-07-10 15:20:38 +0000

    "string introduction "

---
+++ 01_basics/V11_String.js
@@ -0,0 +1,37 @@
+//String
+//
+const name = "Pranil";
+const repoCount = 50;
+console.log(name+repoCount+" Value");
+
+//now we use `` instead of " "  cuase of STRING INTERPOLATION.
+//## STRING INTERPOLATION:
+//we can use other methods with input or other.
+
+console.log(`My name is ${name.toUpperCase()} and repoCount is ${repoCount}`);
+
+//We can declare String by using new keyword.
+
+const user = new String('Anil');
+console.log(user);
+//here we can get .length as well as key pairs to the the above string.
+console.log(user[2]); //key value pairs
+console.log(user.length);
+//Checks at given position what is present
+console.log(user.charAt(1));//i.e, counting starts from 0.
+//to check given element is at position then 
+console.log(user.indexOf('i'));
+
+//substring 
+const name1 = name.substring(2,6); // here includes first value but excludes last value
+console.log(name1);
+
+//slice : we can put -ve values.
+const name2 = name.slice(-6,4);
+console.log(name2);
+
+//trim and replace 
+const user1 = "    RamPrakash   ";
+//trim
+console.log(user1.trim());  // trim removes empty space
+
