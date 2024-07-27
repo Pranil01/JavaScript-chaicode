4573104538d6f351f72160f3887e9f68259c17cc
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2024-07-27 08:35:14 +0000

    objects

---
+++ 02_basics/V18_Object3.js
@@ -0,0 +1,19 @@
+//object destructure
+
+const course= {
+    coursename :"Js in hindi",
+    price:"999",
+    instructor:"Hitesh"
+}
+
+//imp : we don not need to course.instructor instead of it er can write it
+const {instructor}=course;
+console.log(instructor)
+
+//json
+//in simple words it like a object having no name
+
+{
+    "name":"Pranil",
+    "age":"23",
+}
