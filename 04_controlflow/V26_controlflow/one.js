99ae4642d771fcbff482e3eabd035a2935278f1d
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2024-07-31 05:24:52 +0000

    started control flow

---
+++ 04_controlflow/V26_controlflow/one.js
@@ -0,0 +1,22 @@
+//if
+
+// if(condition){
+    
+// }
+//<,>,!,==,>=,<=,!=,===
+//== : It checks only value not datatype
+//But === : It checks both value and datatype
+if(2=='2'){
+    console.log("executed for double")
+}
+if(2==='2'){
+    console.log("executed for tripple")
+}
+
+
+const target =200;
+if(target>100){
+    const power = 150;
+    console.log(`Power is: ${power}`)
+}
+// console.log(`Power is: ${power}`)   Power has bloick scope so it can print only within block
