e29b01e8f6b97a05c5392ce57260c2f98064f66e
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2024-08-28 03:15:58 +0000

    Dom selector and node list

---
+++ 06_Dom/V_32_Dom.js
@@ -0,0 +1,21 @@
+//in dom we find out elements by using below methods
+//document.getElementById('') 
+//document.getElementByName('')
+//document.getElementByclassName('')
+
+//document.getElementById('').id = it gives id 
+//document.getElementById('') .class
+//document.getElementById('').className=it gives classname
+//getAtrribute is function it also give like above
+//document.getElementById('').getAtrribute('id') = it gives id
+//setAttribute is function used to set elements inside class
+//document.getElementById('').setAttribute('class','test')=> above override always
+
+//we can provide style by using className.style.backgroundcolor='red'
+
+//title.InnerHTML            //title.InnerText                    //title.InnerContent
+//it gives including html   //it givees visible values             //it gives invisble i.e, hided values
+
+
+//document.querySelector('h1') = it gives all elements including h1 tag,it gives single list item
+//document.querySelectorAll('li') = it gives all elements including h1 tag,it gives all list item
